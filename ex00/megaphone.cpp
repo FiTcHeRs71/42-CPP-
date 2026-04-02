1089e503b9e3110e17760fff66de82c535b79170
fred <frederic.ducrot@hotmail.com>
2026-04-02 21:10:17 +0200

    save

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cctype>
 
 int main(int argc, char **argv)
 {
@@ -8,13 +9,14 @@ int main(int argc, char **argv)
 	}
 	else
 	{
-		for (int i = 1; argv[i]; i++)
+		for (int i = 1; i < argc; i++)
 		{
-			for (int j = 0; argv[i][j]; j++)
+			std::string str = argv[i];
+			for (int j = 0; j < (int)str.size(); j++)
 			{
 				argv[i][j] = toupper(argv[i][j]);
 			}
-			std::cout << argv[i] << " ";
+			std::cout << argv[i];
 		}
 		std::cout << std::endl;
 	}
