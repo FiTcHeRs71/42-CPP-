129db7b7049e45e1da9b7ce551750e8f950ab57f
fred <frederic.ducrot@hotmail.com>
2026-04-05 11:49:53 +0200

    save

--- cpp_00/ex01/includes/contact.hpp
+++ cpp_00/ex01/includes/contact.hpp
@@ -2,30 +2,35 @@
 # define CONTACT_HPP
 
 #include <iostream>
+#include <string>
 
-class contact
+class Contact
 {
 private:
-	int				_index;
+
 	std::string		_lastName;
 	std::string		_firstName;
 	std::string		_nickName;
+	std::string		_phone;
+	std::string		_secret;
+
 public:
-	contact();
-	~contact();
+	Contact();
+	~Contact();
 
 	// Getters
-	std::string		getLastName(void);
-	std::string		getFirstName(void);
-	std::string		getNickName(void);
-	int				getIndex(void);
+	std::string		getLastName(void)const;
+	std::string		getFirstName(void)const;
+	std::string		getNickName(void)const;
+	std::string		getPhone(void)const;
+	std::string		getSecret(void)const;
 
 	// Setters
-	void			setLastName(std::string LastName);
+	void			setLastName(std::string lastName);
 	void			setFirstName(std::string firstName);
-	void			setNickName(std::string firstName);
-	void			setIndex(int index);
-
+	void			setNickName(std::string nickName);
+	void			setPhone(std::string phone);
+	void			setSecret(std::string secret);
 };
 
 #endif
--- cpp_00/ex01/includes/phonebook.hpp
+++ cpp_00/ex01/includes/phonebook.hpp
@@ -1,16 +1,31 @@
-#ifndef PHONEBOOK_H
-# define PHONEBOOK_H
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
 
+#include <iostream>
+#include <string>
 #include "contact.hpp"
 
-class phonebook
+class PhoneBook
 {
 private:
-	contact _contact[8];
+
+	Contact _contact[8];
 	int		count;
+
 public:
-	phonebook();
-	~phonebook();
+
+	PhoneBook();
+	~PhoneBook();
+
+	//Setters
+	void	addContact(
+		std::string lastName,
+		std::string firstName,
+		std::string nickName,
+		std::string phone,
+		std::string secret
+		);
+	void	search();
 };
 
 
