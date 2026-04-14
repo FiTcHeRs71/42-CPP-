7addcc8ed688d22b8d004883204797dfff19aa4f
Frederic Ducrot <fducrot@c1r3s4.42lausanne.ch>
2026-04-14 15:41:23 +0200

    save

---
+++ cpp_01/ex00/Zombie.hpp
@@ -0,0 +1,18 @@
+
+#ifndef _ZOMBIE_HPP_
+# define _ZOMBIE_HPP_
+
+# include <iostream>
+# include <iomanip>
+
+class Zombie
+{
+private:
+	/* data */
+public:
+	Zombie(/* args */);
+	~Zombie();
+};
+
+
+#endif /*_ZOMBIE_HPP_*/
