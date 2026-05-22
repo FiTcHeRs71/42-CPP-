95f1645e6aee745f93cc3124bbe5348bd5b3aeaa
Frederic Ducrot <fducrot@c1r3s4.42lausanne.ch>
2026-05-22 14:30:13 +0200

    save

---
+++ cpp_05/ex00/Bureaucrat/Bureaucrat.hpp
@@ -0,0 +1,27 @@
+#ifndef __BUREAUCRAT_HPP__
+# define __BUREAUCRAT_HPP__
+
+class Bureaucrat
+{
+	private:
+	
+
+	protected:
+	
+
+	public:
+
+	/*===Canonical Form===*/
+	Bureaucrat(void);
+	~Bureaucrat(void);
+	Bureaucrat(const Bureaucrat& to_copy);
+	Bureaucrat&operator=(const Bureaucrat& src);
+
+	/*===Getters & Setters===*/
+	
+
+	/*===Member Function===*/
+	
+};
+
+#endif /*__BUREAUCRAT_HPP_*/
